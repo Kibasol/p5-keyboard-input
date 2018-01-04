d53db517f2427c7989321606e02dfd772e0980a4
false god <mburns@wlapcs.org>
2018-01-04 00:23:06 +0000

    build keyboard inputs

--- sketch.js
+++ sketch.js
@@ -3,5 +3,23 @@ function setup(){
 }
 
 function draw(){
+    if(keyIsPressed){
+        if(key=="r"){
+        background(255,0,0);
+        }
+        if(key=="g"){
+            background(0,255,0);
+        }
+        if(key=="b"){
+            background(0,0,255);
+        }    
+    }
+    else{
+        background(50);
+    }
+}
+
+
+function keyReleased(){
     
 }
